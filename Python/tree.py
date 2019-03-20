58b1e732539bd0750403d67ee55dec5655686be6
enderson1941 <enderson1941@gmail.com>
2019-03-20 16:10:50 +0900

    1610

--- Python/tree.py
+++ Python/tree.py
@@ -1,7 +1,6 @@
-from turtle import *
-from random import *
 from math import *
-
+from random import *
+from turtle import *
 
 # def tree(n, l):
 #     pd()
@@ -55,18 +54,17 @@ from math import *
 # from math import *
 
 def tree(n, l):
-    pd() # 下笔
+    pd()  # 下笔
     # 阴影效果
     t = cos(radians(heading() + 45)) / 8 + 0.25
     pencolor(t, t, t)
     pensize(n / 3)
-    forward(l) # 画树枝
-
+    forward(l)  # 画树枝
 
     if n > 0:
-        b = random() * 15 + 10 # 右分支偏转角度
-        c = random() * 15 + 10 # 左分支偏转角度
-        d = l * (random() * 0.25 + 0.7) # 下一个分支的长度
+        b = random() * 15 + 10  # 右分支偏转角度
+        c = random() * 15 + 10  # 左分支偏转角度
+        d = l * (random() * 0.25 + 0.7)  # 下一个分支的长度
         # 右转一定角度，画右分支
         right(b)
         tree(n - 1, d)
@@ -80,7 +78,7 @@ def tree(n, l):
         # 画叶子
         right(90)
         n = cos(radians(heading() - 45)) / 4 + 0.5
-        pencolor(n, n*0.8, n*0.8)
+        pencolor(n, n * 0.8, n * 0.8)
         circle(3)
         left(90)
 
@@ -89,46 +87,45 @@ def tree(n, l):
             pu()
             # 飘落
             t = heading()
-            an = -40 + random()*40
+            an = -40 + random() * 40
             setheading(an)
-            dis = int(800*random()*0.5 + 400*random()*0.3 + 200*random()*0.2)
+            dis = int(800 * random() * 0.5 + 400 * random()
+                      * 0.3 + 200 * random() * 0.2)
             forward(dis)
             setheading(t)
-
 
             # 画叶子
             pd()
             right(90)
             n = cos(radians(heading() - 45)) / 4 + 0.5
-            pencolor(n*0.5+0.5, 0.4+n*0.4, 0.4+n*0.4)
+            pencolor(n * 0.5 + 0.5, 0.4 + n * 0.4, 0.4 + n * 0.4)
             circle(2)
             left(90)
             pu()
 
-            #返回
+            # 返回
             t = heading()
             setheading(an)
             backward(dis)
             setheading(t)
 
     pu()
-    backward(l)# 退回
+    backward(l)  # 退回
 
-bgcolor(0.5, 0.5, 0.5) # 背景色
-ht() # 隐藏turtle
-speed(5) # 速度，1-10渐进，0最快
+
+bgcolor(0.5, 0.5, 0.5)  # 背景色
+ht()  # 隐藏turtle
+speed(5)  # 速度，1-10渐进，0最快
 tracer(0, 0)
-pu() # 抬笔
+pu()  # 抬笔
 backward(100)
-left(90) # 左转90度
-pu() # 抬笔
-backward(300) # 后退300
-tree(12, 100) # 递归7层
+left(90)  # 左转90度
+pu()  # 抬笔
+backward(300)  # 后退300
+tree(12, 100)  # 递归7层
 ts = getscreen()
 ts.getcanvas().postscript(file="tree.eps")
 done()
-
-
 
 
 # 作者：杨航锋
