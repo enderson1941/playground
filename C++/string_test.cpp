c8009e105af01af6b0e7267ad6b1d0e2b108af90
enderson1941 <enderson1941@gmail.com>
2019-03-26 15:36:55 +0900

    1536

---
+++ C++/string_test.cpp
@@ -0,0 +1,26 @@
+#include "iostream"
+#include "stdio.h"
+#include "vector"
+#include "ostream"
+#include "time.h"
+#include "vector"
+#include "windows.h"
+#include "io.h"
+#include "iomanip"
+#include "string.h"
+#include "sstream"
+#include "stdlib.h"
+#include "fstream"
+#include "string.h"
+
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    cout << a << endl;
+    // string st = "abc";
+    std::string st;
+    cin >> st;
+    return 0;
+}
